771fb54cbffc596c8f3be5e05baff7d4c6084d6a
MayukhGautam <54997204+MayukhGautam@users.noreply.github.com>
2019-12-10 13:07:36 -0500

    Update sketch.js

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,8 +1,12 @@
 function setup() {
-  createCanvas(200,200);
+  var cnv = createCanvas(300, 300);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  background(255, 0, 200);
 }
 
 function draw() {
-  background(200);
-  ellipse(0,0,200);
+  background(200,200,100);
+  ellipse(150,150,10);
 }
