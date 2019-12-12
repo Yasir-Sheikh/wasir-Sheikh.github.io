c545c3e3135d77db7b91b7fb71d32a1a4639991f
MayukhGautam <54997204+MayukhGautam@users.noreply.github.com>
2019-12-12 12:55:09 -0500

    Update sketch.js

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,11 +1,16 @@
 function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
+  //cnv.parent("a");
   //let x = (windowWidth - width) / 2;
   //let y = (windowHeight - height) / 2;
   //cnv.position(x, y);
 
 }
-let pix;
+
+let hPix;
+let pixChoice1= [0,0,0];
+let pixChoice2= [0,0,0];
+let clickNum = 0;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -13,20 +18,32 @@ function windowResized() {
 
 function draw() {
   background(200, 200, 100);
-  fill(255,0,0);
+  fill(0, 0, 255);
   strokeWeight(10);
-  ellipse(200,200,200);
-  if (mouseIsPressed) {
-    if (mouseButton === LEFT) {
-      //console.log("X: " + clickX);
-      //console.log("Y: " + clickY);
-      pix = get(mouseX, mouseY);
-      for(let i = 0; i<3; i++){
-      fill(0);
-      text(pix[i], (windowWidth/2)+ i*30, windowHeight/2);
-      }
+  ellipse(200, 200, 200);
 
-
-    }
+  hPix = get(mouseX, mouseY);
+  for (let i = 0; i < 3; i++) {
+    fill(0);
+    text(hPix[i], (windowWidth / 2) + i * 30, windowHeight / 2);
+    text(pixChoice1[i], 0 + i * 30, 10)
+    text(pixChoice2[i], 0 + i * 30, 40)
   }
+
+
+}
+
+function mouseClicked() {
+
+    if(clickNum%2 === 0)
+    {
+      pixChoice1 = get(mouseX, mouseY);
+    }else
+    {
+      pixChoice2 = get(mouseX, mouseY);
+      //console.log(pixChoice2);
+    }
+  clickNum++;
+  console.log(clickNum);
+
 }
