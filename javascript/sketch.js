a8baa060d7b156f25b7d3c031ecfd9c06dbadf4c
Yasir-Sheikh <55155008+Yasir-Sheikh@users.noreply.github.com>
2019-12-12 21:52:01 -0500

    Update sketch.js

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,8 +1,6 @@
-let grad;
-
 function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
-  grad = loadImage("img/larsV.jpg");
+  //grad = loadImage("img/larsV.jpg");
   //cnv.parent("a");
   //let x = (windowWidth - width) / 2;
   //let y = (windowHeight - height) / 2;
@@ -12,9 +10,11 @@ function setup() {
 
 
 let hPix;
-let pixChoice1= [0,0,0];
-let pixChoice2= [0,0,0];
+let pixChoice1 = [0, 0, 0];
+let pixChoice2 = [0, 0, 0];
 let clickNum = 0;
+let newColor = [255, 255, 255];
+let n = 1;
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -25,8 +25,10 @@ function draw() {
   fill(0, 0, 255);
   strokeWeight(10);
   ellipse(200, 200, 200);
+  fill(newColor[0], newColor[1], newColor[2]);
+  rect(500, 100, 200, 200);
 
-  image(grad, 0, 0, 300, 300);
+  //image(grad, 0, 0, 300, 300);
 
   hPix = get(mouseX, mouseY);
   for (let i = 0; i < 3; i++) {
@@ -35,19 +37,29 @@ function draw() {
     text(pixChoice1[i], 0 + i * 30, 10)
     text(pixChoice2[i], 0 + i * 30, 40)
   }
+  if (clickNum === 2 * n) {
+    mix(pixChoice1, pixChoice2, newColor);
+    n++;
+  }
+  text(newColor[0] + "   " + newColor[1] + "       " + newColor[2], 0, 70);
+}
+
+function mix(color1, color2, color3) {
+
+  color3[0] = (color1[0] + color2[0]) / 2
+  color3[1] = (color1[1] + color2[1]) / 2
+  color3[2] = (color1[2] + color2[2]) / 2
 
 }
 
 function mouseClicked() {
 
-    if(clickNum%2 === 0)
-    {
-      pixChoice1 = get(mouseX, mouseY);
-    }else
-    {
-      pixChoice2 = get(mouseX, mouseY);
-      //console.log(pixChoice2);
-    }
+  if (clickNum % 2 === 0) {
+    pixChoice1 = get(mouseX, mouseY);
+  } else {
+    pixChoice2 = get(mouseX, mouseY);
+    //console.log(pixChoice2);
+  }
   clickNum++;
   console.log(clickNum);
 
